2bb2e0d63fd86258951011bab84c47dd356d01c7
pante <panterua@hotmail.com>
2020-08-11 14:24:30 -0300

    Agregado del pseudologin

--- js/login.js
+++ js/login.js
@@ -1,6 +1,40 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+let loginInputs = [];
 
+const checkInputs = () => {
+  let filledForm = true;
+
+  loginInputs.forEach( input => {
+    if (input.value === '') {
+      filledForm = false;
+      input.style.background = '#e6c2c2';
+    }
+  });
+
+  if (filledForm) {
+    window.location = 'index.html';
+  }
+}
+
+const removeWarning = () => {
+  loginInputs.forEach( input => {
+    if (input.value != '') {
+      input.style.background = 'none';
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+  loginInputs = [
+    document.getElementById('inputUser'),
+    document.getElementById('inputPassword')
+  ];
+  
+  // Revisa los inputs si se clickea en 'Entrar' o si se pulsa la tecla 'Enter'
+  document.getElementById("submitButton").addEventListener('click', checkInputs);
+  document.getElementById("loginForm").addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') checkInputs();
+  });
+
+  // Si el usuario empezó a llenar un campo, entonces remuevo la advertencia visual de este
+  document.getElementById('loginForm').addEventListener('keyup', removeWarning);
 });
