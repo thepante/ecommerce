fcc3c697047afb2123be4520117b2f1749f0e0b2
thepante <panterua@hotmail.com>
2020-08-12 13:36:38 -0300

    Pequeño ajuste de estilo

--- js/products.js
+++ js/products.js
@@ -10,11 +10,11 @@ function showProductsList(array){
       <div class="list-group-item list-group-item-action">
           <div class="row">
               <div class="col-3">
-                  <img src="${product.imgSrc}" alt="${product.description}" class="img-thumbnail">
+                  <img src="${product.imgSrc}" alt="${product.name}" class="img-thumbnail">
               </div>
               <div class="col">
                   <div class="d-flex w-100 justify-content-between">
-                      <h4 class="mb-1"><span class="badge badge-secondary">${product.currency} ${product.cost}</span> ${product.name}</h4>
+                      <h4 class="mb-1"><span class="badge badge-secondary">${product.currency} ${product.cost}</span> <strong>${product.name}</strong></h4>
                       <small class="text-muted">${product.soldCount} vendidos</small>
                   </div>
                   <p>${product.description}</p>
