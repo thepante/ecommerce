6bde923cfd7b6d82863926fa99fed346778cc59c
pante <panterua@hotmail.com>
2020-08-17 18:28:34 -0300

    Agregado filtrado de productos por precio

--- js/products.js
+++ js/products.js
@@ -1,29 +1,34 @@
 var productsArray = [];
 
+let minPrice = 0;
+let maxPrice = Infinity;
+
 function showProductsList(array){
     let htmlContentToAppend = "";
     
     for (let i = 0, l = array.length; i < l; i++) {
       let product = array[i];
 
-      htmlContentToAppend += `
-      <div class="list-group-item list-group-item-action">
-          <div class="row">
-              <div class="col-3">
-                  <img src="${product.imgSrc}" alt="${product.name}" class="img-thumbnail">
-              </div>
-              <div class="col">
-                  <div class="d-flex w-100 justify-content-between">
-                      <h4 class="mb-1"><span class="badge badge-secondary">${product.currency} ${product.cost}</span> <strong>${product.name}</strong></h4>
-                      <small class="text-muted">${product.soldCount} vendidos</small>
-                  </div>
-                  <p>${product.description}</p>
-              </div>
-          </div>
-      </div>
-      `
+      if ((product.cost >= minPrice) && (product.cost <= maxPrice)) {
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="${product.imgSrc}" alt="${product.name}" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1"><span class="badge badge-secondary">${product.currency} ${product.cost}</span> <strong>${product.name}</strong></h4>
+                        <small class="text-muted">${product.soldCount} vendidos</small>
+                    </div>
+                    <p>${product.description}</p>
+                </div>
+            </div>
+        </div>
+        `;
+      }
     };
-    
+
     // Lo agrego al finalizar el loop para no tener que hacerlo repetitivamente en cada iteración
     document.getElementById("products-list-container").innerHTML = htmlContentToAppend;
 }
@@ -33,10 +38,50 @@ function showProductsList(array){
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
+        if (resultObj.status === "ok") {
             productsArray = resultObj.data;
             showProductsList(productsArray);
         }
+    });
+
+    // Filtrar resultados
+    document.getElementById("rangeFilter").addEventListener("click", () => {
+      let rangeMin = document.getElementById("rangePriceMin").value;
+      let rangeMax = document.getElementById("rangePriceMax").value;
+
+      // No aceptar que el min sea igual o mayor al max
+      if (parseInt(rangeMin) >= parseInt(rangeMax)) {
+        document.getElementById("rangePriceMin").value = null;
+        document.getElementById("rangePriceMax").value = null;
+        alert("El mínimo debe ser menor al máximo");
+        return;
+      }
+
+      // Doble check de no aceptar valores negativos (en el HTML se invalidó esta posibilidad)
+      if (((parseInt(rangeMin) || parseInt(rangeMax)) < 0)) {
+        document.getElementById("rangePriceMin").value = null;
+        document.getElementById("rangePriceMax").value = null;
+        alert("No pueden ser negativos");
+        return;
+      }
+
+      // Tomar min
+      minPrice = (parseInt(rangeMin)) ? parseInt(rangeMin) : 0;
+
+      // Tomar max
+      maxPrice = (parseInt(rangeMax)) ? parseInt(rangeMax) : Infinity;
+
+      showProductsList(productsArray);
+    });
+
+    // Limpiar el filtrado
+    document.getElementById("clearFilter").addEventListener("click", () => {
+      minPrice = 0;
+      maxPrice = Infinity;
+
+      document.getElementById("rangePriceMin").value = null;
+      document.getElementById("rangePriceMax").value = null;
+
+      showProductsList(productsArray);
     });
 });
