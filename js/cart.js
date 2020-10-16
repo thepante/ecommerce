2fc7ad92f5b3c02fe3632b8d7a512bd7afc87af0
pante <panterua@hotmail.com>
2020-10-16 17:03:22 -0300

    funcionalidad del checkout
    Para continuar con el pago requiere haber especificado la dirección y elegir un método de pago. El botón se habilita solamente si están todos los datos necesarios. Se vuelve a verificar que estén todos los datos (incluso los precios) para poder realizar la compra

--- js/cart.js
+++ js/cart.js
@@ -11,6 +11,23 @@ const shippingPrice = document.getElementById('shipping-price');
 const couponInput = document.getElementById('coupon-code');
 const discounted = document.getElementById('discount-amount');
 
+const SHIPPING_METHODS = {
+  standard: {
+    cost: 5,
+    days: [12, 15],
+  },
+  express: {
+    cost: 7,
+    days: [5, 8],
+  },
+  premium: {
+    cost: 15,
+    days: [2, 5],
+  },
+};
+
+let SELECTED_SHIPPING_METHOD = null;
+let SELECTED_PAYMENT_METHOD = null;
 let SELECTED_CURRENCY = null;
 let couponCode = null;
 let articles = [];
@@ -68,6 +85,13 @@ function changeAmount(e, step) {
   calcSubtotal(row.id);
 }
 
+// quita un producto del carrito
+function removeItem(e) {
+  const index = Number(e.parentNode.parentNode.id.match(/\d/)[0]);
+  articles.splice(index, 1);
+  showCartProducts();
+}
+
 // svgs para los botones de cantidad
 const btnIcon = {
   plus: `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -80,14 +104,15 @@ const btnIcon = {
 
 // mostrar productos en el carrito
 function showCartProducts() {
-  let html = '';
+  const cartPage = document.getElementById('cart');
+  const cartList = document.getElementById('cart-articles');
 
-  articles.forEach((product, i) => {
+  const cartProductsHtml = articles.map((product, i) => {
     let {name, count, unitCost, currency, src} = product;
     let price = processPrice(currency, unitCost); // cambia de moneda si es necesario
     let subtotal = count * price;
 
-    html += `
+    return `
       <tr id="item${i}">
         <td><img src="${src}" /></td>
         <td>${name}</td>
@@ -98,13 +123,26 @@ function showCartProducts() {
           <button onclick="changeAmount(this, 1)">${btnIcon.plus}</button>
         </td>
         <td class="price subtotal">${formatPrice(subtotal)}</td>
+        <td><span onclick="removeItem(this)" title="Quitar '${name}' del carrito"><i class="fas fa-times"></i></span></td>
       </tr>
     `;
-  });
+  }).join('');
 
-  itemsAmount.innerText = articles.length;
-  document.getElementById('cart-articles').innerHTML = html;
-  calcProductsTotal();
+  const showEmptyCart = `
+    <div class="empty-cart">
+      <h4><strong>Tu carrito se encuentra vacío</strong></h4> <br>
+      <p>No tienes productos en tu carrito. <a href="./products.html">Continúa comprando!</a>
+    </div>
+  `;
+
+  // mostrar lo que corresponda
+  if (articles.length > 0) {
+    itemsAmount.innerText = articles.length;
+    cartList.innerHTML = cartProductsHtml;
+    calcProductsTotal();
+  } else {
+    cartPage.innerHTML = showEmptyCart;
+  }
 }
 
 
@@ -155,17 +193,15 @@ function calcFinalCost() {
 
 // el costo de envío es un porcentaje del costo total de los productos (sin tomar en cuenta el descuento)
 function calcShipping() {
-  const costs = {
-    standard: 1,
-    express: 3,
-  };
-
-  const selected = document.querySelector('#shipping input[name="shippingMethod"]:checked').value;
+  SELECTED_SHIPPING_METHOD = document.querySelector('#shipping input[name="shippingMethod"]:checked').value;
   const productsPrice = extractPrice(productsTotal);
-  const shippingCost = productsPrice * (costs[selected] / 100);
+  const shippingCost = productsPrice * (SHIPPING_METHODS[SELECTED_SHIPPING_METHOD].cost / 100);
+
+  document.getElementById('shipping-from').innerText = SHIPPING_METHODS[SELECTED_SHIPPING_METHOD].days[0];
+  document.getElementById('shipping-until').innerText = SHIPPING_METHODS[SELECTED_SHIPPING_METHOD].days[1];
+  document.getElementById('shipping-cost').innerText = SHIPPING_METHODS[SELECTED_SHIPPING_METHOD].cost;
 
   shippingPrice.innerText = formatPrice(shippingCost);
-  document.getElementById('show-cost').innerText = costs[selected];
   calcFinalCost();
 }
 
@@ -195,6 +231,247 @@ function calcDiscount() {
   calcFinalCost();
 }
 
+// manejo de la sección de 'datos de envío'
+const shippingInfo = {
+  KEY: 'shipping-info',
+  INFO: document.getElementById('shipping-info'),
+  EDIT: document.getElementById('address-edit'),
+  country: document.getElementById('address-country'),
+  street: document.getElementById('address-st'),
+  number: document.getElementById('address-number'),
+  corner: document.getElementById('address-corner'),
+
+  // validar y guardar los datos de dirección
+  save() {
+    const inputs = [this.country, this.street, this.number, this.corner];
+    let isValid = inputs.every(input => input.value.trim() !== '');
+
+    if (isValid) {
+      const data = {
+        country: this.country.value,
+        street: this.street.value,
+        number: this.number.value,
+        corner: this.corner.value,
+      };
+
+      localStorage.setItem(this.KEY, JSON.stringify(data));
+      this.display();
+    }
+  },
+
+  // cargar los datos desde localStorage
+  load() {
+    const {country, street, number, corner} = JSON.parse(localStorage.getItem(this.KEY));
+    this.country.value = country;
+    this.street.value = street;
+    this.number.value = number;
+    this.corner.value = corner;
+  },
+
+  // mostrar los datos de dirección
+  display() {
+    this.load();
+    this.INFO.innerHTML = `
+      <div id="edit-address" onclick="shippingInfo.toggle()" title="Editar dirección"><i class="fas fa-edit"></i></div>
+      ${this.country.value}: <br>
+      ${this.street.value} ${this.number.value}, ${this.corner.value}
+    `;
+    document.getElementById('cancel-edit-address').style.display = 'inline';
+    this.INFO.style.display = 'inline';
+    this.EDIT.style.display = 'none';
+    setCheckoutBtnStatus();
+  },
+
+  // cambia de vista (edición o visualización de los datos)
+  toggle() {
+    this.load();
+    [this.EDIT, this.INFO].forEach(e => e.style.display = e.style.display === 'none' ? 'inline' : 'none');
+  },
+}
+
+// verifica si están todos los datos previos para continuar con el checkout
+function isShippingAndPaymentValid() {
+  let valid = null;
+
+  const radios = [
+    document.getElementsByName('shippingMethod'),
+    document.getElementsByName('paymentMethod'),
+  ];
+
+  // validar radios
+  valid = radios.every(group => Array.from(group).some(radio => radio.checked));
+
+  // validar la data en localStorage
+  const storedInfo = JSON.parse(localStorage.getItem(shippingInfo.KEY));
+  if (valid) valid = storedInfo ? Object.values(storedInfo).every(value => value.trim() !== '') : false;
+
+  return valid;
+}
+
+// establecer disponibilidad de continuar con el pago
+function setCheckoutBtnStatus() {
+  const isDataFilled = isShippingAndPaymentValid();
+  document.getElementById('proceedCheckout').disabled = !isDataFilled;
+}
+
+// mostrar el formulario de pago que corresponda
+function enablePaymentForm(selected) {
+  SELECTED_PAYMENT_METHOD = selected;
+
+  const paymentForms = {
+    card: document.getElementById('checkout-card'),
+    transfer: document.getElementById('checkout-transfer'),
+  };
+
+  for (const [method, element] of Object.entries(paymentForms)) {
+    element.style.display = SELECTED_PAYMENT_METHOD === method ? 'inline' : 'none';
+  };
+
+  setCheckoutBtnStatus();
+}
+
+// habilitar el pago si están todos los datos necesarios
+// solo revisa que no estén vacíos, nada más
+function confirmPayment() {
+  const paymentForm = document.getElementById(`checkout-${SELECTED_PAYMENT_METHOD}`);
+
+  const validSaP = isShippingAndPaymentValid();
+  const validAmounts = Array.from(document.querySelectorAll('#cart-articles input[type="number"]')).every(e => Number(e.value) > 0);
+  const validPrices = Array.from(document.getElementsByClassName('price')).every(e => e.innerText.trim() !== '');
+  const validPayment = Array.from(paymentForm.getElementsByTagName('input')).every(e => e.value.trim() !== '');
+
+  console.log("A", validAmounts);
+  console.log("B", validPrices)
+  console.log("C", validSaP);
+  console.log("D", validPayment);
+
+  if (validSaP && validAmounts && validPrices && validPayment) showBuySuccess();
+}
+
+// confirma la compra y muestra un resumen de la misma
+function showBuySuccess() {
+
+  // datos de envío
+  const {country, street, number, corner} = {
+    country: shippingInfo.country.value,
+    street: shippingInfo.street.value,
+    number: shippingInfo.number.value,
+    corner: shippingInfo.corner.value,
+  };
+
+  // datos de los productos
+  const {pricesTotal, discount, shippingCost, finalTotal} = {
+    pricesTotal: document.getElementById('pricesTotal').innerText,
+    discount: document.getElementById('discount-amount').innerText,
+    shippingCost: document.getElementById('shipping-price').innerText,
+    finalTotal: document.getElementById('final-cost').innerText,
+  };
+
+  const shipping = SHIPPING_METHODS[SELECTED_SHIPPING_METHOD];
+  const modalContent = document.querySelector('#checkoutModal .modal-content');
+
+  const today = new Date();
+  const day = `${today.getDate()}-${today.getMonth() + 1}-${today.getFullYear()}`;
+  const time = today.toLocaleTimeString([], { hour12: false, hour: '2-digit', minute: '2-digit' });
+  const datetime = `${day} / ${time}`;
+
+  // animación de espera
+  modalContent.innerHTML = `
+    <div class="processing-payment">
+      <div class="sk-folding-cube">
+        <div class="sk-cube1 sk-cube"></div>
+        <div class="sk-cube2 sk-cube"></div>
+        <div class="sk-cube4 sk-cube"></div>
+        <div class="sk-cube3 sk-cube"></div>
+      </div>
+      <p>Procesando pago...</p>
+    </div>
+  `;
+
+  // html del listado de productos
+  const productsListHTML = Array.from(document.querySelectorAll('tr[id^="item"]')).map(product => {
+    const {name, price, amount, subtotal} = {
+      name: product.querySelector('td:nth-of-type(2)').innerText,
+      price: product.querySelector('.unitCost').innerText,
+      amount: product.querySelector('input').value,
+      subtotal: product.querySelector('.subtotal').innerText,
+    };
+
+    return `
+      <tr>
+        <td>${name}<br><small class="badge badge-light text-muted">${price}</small></td>
+        <td>x${amount}</td>
+        <td class="price">${subtotal}</td>
+      </tr>
+    `;
+  }).join('');
+
+  // éxito del pago
+  const successHtml = `
+    <div class="modal-header">
+      <h5 class="modal-title" id="checkoutModalLabel">Compra realizada con éxito</h5>
+      <button type="button" class="close" data-dismiss="modal" onclick="closeConfirmation()" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div id="receipt" class="modal-body receipt">
+      <p class="alert-success">
+        ${userData.username}, gracias por su compra! <br>
+        <strong>Su pedido será enviado en ${country} a:</strong> <br>
+        ${street} N°${number}, esq. ${corner} <br>
+        Le llegará dentro de ${shipping.days[0]} a ${shipping.days[1]} días por envío ${SELECTED_SHIPPING_METHOD}.
+      </p>
+      <table class="table products-summary">
+        <tr>
+          <th width="60%">Producto</th>
+          <th width="10%">Cantidad</th>
+          <th width="30%">Subtotal</th>
+        </tr>
+        ${productsListHTML}
+      </table>
+      <table class="table costs-summary">
+        <tr><th width="50%">Totales</th><th width="50%"></th></tr>
+        <tr><td>Productos:</td><td class="price">${pricesTotal}</td></tr>
+        <tr><td>Descuento:</td><td class="price">${discount}</td></tr>
+        <tr><td>Costo de envío:</td><td class="price">${shippingCost}</td></tr>
+        <tr><th>Total final:</th><th class="price">${finalTotal}</th></tr>
+      </table>
+      <hr>
+      <div class="datetime">Fecha y hora de compra: ${datetime}</div>
+    </div>
+    <div class="modal-footer text-center">
+      <button type="button" onclick="window.location = './categories.html'" class="btn btn-light ml-auto"><i class="fas fa-shopping-cart"></i> Continuar comprando</button>
+      <button type="button" onclick="printReceipt()" class="btn btn-light mr-auto"><i class="fas fa-file-invoice"></i> Descargar recibo</button>
+    </div>
+  `;
+
+  // simular espera de respuesta del backend y luego mostrar resultado
+  setTimeout(() => modalContent.innerHTML = successHtml, 2200);
+}
+
+
+// para que el botón no quede pintado ahí xD funcionalidad del 'descargar recibo'
+function printReceipt() {
+  const print = window.open('', 'PRINT', 'height=750,width=900,top=100,left=150');
+  print.document.write(`
+    <html><head><title>Recibo eMercado</title></head><body>
+    ${document.getElementById('receipt').innerHTML}
+    </body></html>
+  `);
+
+  print.document.close();
+  print.focus();
+  print.print();
+  print.close();
+  return true;
+}
+
+// ciere de confirmación de compra
+function closeConfirmation() {
+  articles = [];
+  showCartProducts();
+}
+
 
 document.addEventListener("DOMContentLoaded", function(e){
   // reviso si ya había preferencia de la moneda
@@ -213,4 +490,29 @@ document.addEventListener("DOMContentLoaded", function(e){
       showCartProducts();
     }
   });
+
+  // datos de envío: mostrar la sección que corresponda
+  localStorage.getItem(shippingInfo.KEY) ? shippingInfo.display() : shippingInfo.EDIT.style.display = 'inline';
+
+  // guardar datos de envío
+  document.getElementById('save-shipping-info').addEventListener('click', () => shippingInfo.save());
+
+  // asignar el formulario del pago seleccionado
+  document.getElementById('payment-method').addEventListener('change', function() {
+    const method = this.querySelector('input:checked').value;
+    enablePaymentForm(method);
+  });
+
+  // establecer estado al botón de checkout
+  setCheckoutBtnStatus();
+
+  // const checkout = document.getElementById('form-checkout');
+  // checkout.addEventListener('submit', function (e) {
+  //   if (checkout.checkValidity() === false) {
+  //     e.preventDefault();
+  //     e.stopPropagation();
+  //   }
+  //   checkout.classList.add('was-validated');
+  // });
+
 });
