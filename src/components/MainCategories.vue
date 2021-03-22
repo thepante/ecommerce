48e8c49a350fb1c01db13756dc66de2368198091
pante <panterua@hotmail.com>
2021-03-22 09:23:22 -0300

    content placeholders adjusts

--- src/components/MainCategories.vue
+++ src/components/MainCategories.vue
@@ -18,17 +18,17 @@
           :key="index"
           class="col-lg-6"
           width="555"
-          height="292"
+          height="312"
         >
           <rect x="30" y="56" rx="2" ry="2" width="120" height="22" />
           <rect x="30" y="98" rx="0" ry="0" width="215" height="16" />
           <rect x="214" y="81" rx="0" ry="0" width="1" height="0" />
           <rect x="935" y="12" rx="0" ry="0" width="80" height="12" />
           <rect x="30" y="124" rx="0" ry="0" width="180" height="16" />
-          <rect x="0" y="0" rx="0" ry="0" width="555" height="2" />
-          <rect x="0" y="290" rx="0" ry="0" width="555" height="2" />
-          <rect x="0" y="0" rx="0" ry="0" width="2" height="292" />
-          <rect x="553" y="0" rx="0" ry="0" width="2" height="292" />
+          <rect x="0" y="0" rx="5" ry="5" width="555" height="4" />
+          <rect x="0" y="288" rx="5" ry="5" width="555" height="4" />
+          <rect x="0" y="0" rx="5" ry="5" width="4" height="292" />
+          <rect x="551" y="0" rx="5" ry="5" width="4" height="292" />
         </ContentLoader>
       </template>
 
